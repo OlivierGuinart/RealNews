3c6069edcb7a44f25f50d10506b16769c4eb0b54
Olivier Guinart <olivier_guinart+github@live.com>
2025-02-23 18:43:09 +0100

    Added a couple of tests for null objects or invalid indexes

--- RealNews/Forms/myListBox.cs
+++ RealNews/Forms/myListBox.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.Reflection;
+using System.Runtime.Remoting.Contexts;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RealNews
 {
@@ -26,88 +30,101 @@ namespace RealNews
                 return;
 
             var lb = this;
-            var fi = lb.Items[e.Index] as FeedItem;
-            var t = fi.Title;
-            var f = e.Font;
-            //if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
-            //{
-            //    e = new DrawItemEventArgs(e.Graphics,
-            //                  e.Font,
-            //                  e.Bounds,
-            //                  e.Index,
-            //                  e.State ^ DrawItemState.Selected,
-            //                  e.ForeColor,
-            //                  Color.DarkOrchid);//Choose the color
-            //}
-
-            e.DrawBackground();
-
-            //var ItemMargin = 0;
-            Brush myBrush = new SolidBrush(this.ForeColor);
-            if (fi.isRead == false)
+            // System.ArgumentOutOfRangeException: InvalidArgument=Value of '-1' is not valid for 'index'.
+            // This happens if you add a new feed, then update it, and click on the lists of posts right away
+            if (e.Index > -1)
             {
-                // List of post from a RSS show up white on white otherwise
-                myBrush = Settings.DarkMode ? new SolidBrush(Color.White) : new SolidBrush(Color.Black);
-                f = new Font(f, FontStyle.Bold);
-            }
-            var sf = StringFormat.GenericTypographic;
-            sf.Trimming = StringTrimming.EllipsisCharacter;
-            sf.LineAlignment = StringAlignment.Center;
-            int l = 0;
-            int w = 20;
-            if (this.RightToLeft == RightToLeft.Yes)
-            {
-                sf = new StringFormat();
-                //sf.Alignment = StringAlignment.Far;
+                var fi = lb.Items[e.Index] as FeedItem;
+                var t = fi.Title;
+                var f = e.Font;
+                //if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                //{
+                //    e = new DrawItemEventArgs(e.Graphics,
+                //                  e.Font,
+                //                  e.Bounds,
+                //                  e.Index,
+                //                  e.State ^ DrawItemState.Selected,
+                //                  e.ForeColor,
+                //                  Color.DarkOrchid);//Choose the color
+                //}
+
+                e.DrawBackground();
+
+                //var ItemMargin = 0;
+                Brush myBrush = new SolidBrush(this.ForeColor);
+                if (fi.isRead == false)
+                {
+                    // List of posts from a RSS show up white on white otherwise
+                    myBrush = Settings.DarkMode ? new SolidBrush(Color.White) : new SolidBrush(Color.Black);
+                    f = new Font(f, FontStyle.Bold);
+                }
+                var sf = StringFormat.GenericTypographic;
                 sf.Trimming = StringTrimming.EllipsisCharacter;
                 sf.LineAlignment = StringAlignment.Center;
-                sf.FormatFlags = StringFormatFlags.DirectionRightToLeft | StringFormatFlags.NoWrap;
-                l = 20;
-                w = 0;
-            }
-            if (fi.Id == "")
-            {
-                var c = GroupColor;
-                myBrush = new SolidBrush(c);
-                sf = new StringFormat();
-                sf.Alignment = StringAlignment.Center;
-                sf.LineAlignment = StringAlignment.Center;
-                e.Graphics.DrawLine(new Pen(c), e.Bounds.Left + l, e.Bounds.Bottom - 5, e.Bounds.Width - w, e.Bounds.Bottom - 5);
-            }
-            var m = 10;
-            var b = new Rectangle(e.Bounds.X + m, e.Bounds.Y, e.Bounds.Width - 2 * m, e.Bounds.Height);
-            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
-            {
-                // Selected. Draw with the system highlight color.
-                e.Graphics.DrawString(t, e.Font, new SolidBrush(HighLightText), b, sf);
-            }
-            else
-            {
-                e.Graphics.DrawString(t, f, myBrush, b, sf);
-            }
+                int l = 0;
+                int w = 20;
+                if (this.RightToLeft == RightToLeft.Yes)
+                {
+                    sf = new StringFormat();
+                    //sf.Alignment = StringAlignment.Far;
+                    sf.Trimming = StringTrimming.EllipsisCharacter;
+                    sf.LineAlignment = StringAlignment.Center;
+                    sf.FormatFlags = StringFormatFlags.DirectionRightToLeft | StringFormatFlags.NoWrap;
+                    l = 20;
+                    w = 0;
+                }
+                if (fi.Id == "")
+                {
+                    var c = GroupColor;
+                    myBrush = new SolidBrush(c);
+                    sf = new StringFormat();
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
+                    e.Graphics.DrawLine(new Pen(c), e.Bounds.Left + l, e.Bounds.Bottom - 5, e.Bounds.Width - w, e.Bounds.Bottom - 5);
+                }
+                var m = 10;
+                var b = new Rectangle(e.Bounds.X + m, e.Bounds.Y, e.Bounds.Width - 2 * m, e.Bounds.Height);
+                if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                {
+                    // Selected. Draw with the system highlight color.
+                    e.Graphics.DrawString(t, e.Font, new SolidBrush(HighLightText), b, sf);
+                }
+                else
+                {
+                    e.Graphics.DrawString(t, f, myBrush, b, sf);
+                }
 
-            e.DrawFocusRectangle();
+                e.DrawFocusRectangle();
+            }
         }
 
         private void MyListBox_KeyDown(object sender, KeyEventArgs e)
         {
-            if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.Up) && e.Shift == false && e.Alt == false)
+            // System.ArgumentOutOfRangeException  Message = InvalidArgument = Value of '-1' is not valid for 'index'.
+            if (SelectedIndex > -1)
             {
-                var o = Items[SelectedIndex] as FeedItem;
-                o.isRead = true;
-                //MoveNext();
-                //e.SuppressKeyPress = true;
-            }
+                if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.Up) && e.Shift == false && e.Alt == false)
+                {
+                    var o = Items[SelectedIndex] as FeedItem;
+                    o.isRead = true;
+                    //MoveNext();
+                    //e.SuppressKeyPress = true;
+                }
 
-            EnsureVisible(10);
+                EnsureVisible(10);
+            }
         }
 
         private void MyListBox_MouseClick(object sender, MouseEventArgs e)
         {
-            var o = this.SelectedItem as FeedItem;
-            if (o.Id == "")
+            // System.NullReferenceException. Happens when you click on a post that's not there yet (new feed added, no items yet)
+            if (this.SelectedItem != null)
             {
-                // FIX : select all in group
+                var o = this.SelectedItem as FeedItem;
+                if (o.Id == "")
+                {
+                    // FIX : select all in group
+                }
             }
         }
 
