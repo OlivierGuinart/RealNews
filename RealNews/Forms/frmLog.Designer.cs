fb2727ea2441d023d295862a0ada5fe1d2b27fa5
Olivier Guinart <olivier_guinart+github@live.com>
2025-03-15 19:19:39 +0100

    Allowing for control to have a horizontal scrollbar, to see full extent of log lines

--- RealNews/Forms/frmLog.Designer.cs
+++ RealNews/Forms/frmLog.Designer.cs
@@ -49,6 +49,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
             this.listBox1.ItemHeight = 15;
             this.listBox1.Location = new System.Drawing.Point(11, 45);
             this.listBox1.Name = "listBox1";
